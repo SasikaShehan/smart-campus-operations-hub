feb9a95500860e2cffd9cf26fca4511bdf5edef0
Sasika Shehan <166682166+SasikaShehan@users.noreply.github.com>
2026-04-23 17:41:12 +0530

    Delete frontend directory

--- frontend/src/services/auth.js
+++
@@ -1,9 +0,0 @@
-export const getToken = () => localStorage.getItem('token');
-export const setToken = (token) => localStorage.setItem('token', token);
-export const removeToken = () => localStorage.removeItem('token');
-export const isAuthenticated = () => !!getToken();
-
-// Store user info after login
-export const setUser = (user) => localStorage.setItem('user', JSON.stringify(user));
-export const getUser = () => JSON.parse(localStorage.getItem('user'));
-export const removeUser = () => localStorage.removeItem('user');
